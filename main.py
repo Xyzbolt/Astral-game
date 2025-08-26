52601e197683cbd9878b0a970e245f5e115d58a4
Xyzbolt <Xyzzsupreme@gmail.com>
2025-08-26 16:23:09 +0500

    pygame initialised

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import pygame
-from constants import *
+from constants import * # pyright: ignore
 
 def main():
-    print(SCREEN_WIDTH)
-    print(SCREEN_HEIGHT)
+    pygame.init()
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill(color=pygame.Color("black"))
+        pygame.display.flip()
+        
 
 if __name__ == "__main__":
     main()
